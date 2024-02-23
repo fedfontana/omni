3e8c84fa23e3a1fce61b9a7c01fde7ee4f14a10a
fedfontana <federico.fontana2000@gmail.com>
2024-02-23 12:24:54 +0100

    Move validation to clap

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,6 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, str::FromStr};
 
-use clap::Parser;
+use clap::{error::ErrorKind, CommandFactory, Parser};
 
 use crate::format::Format;
 
@@ -13,11 +13,85 @@ pub struct Args {
 
     /// If specified, the input will be treated as this format (has precedence over the inference
     /// from the file extension)
-    #[clap(long)]
+    #[clap(long, alias = "if")]
     pub in_format: Option<Format>,
 
     /// If specified, the output will be treated as this format (has precedence over the inference
     /// from the file extension)
-    #[clap(long)]
+    #[clap(long, alias = "of")]
     pub out_format: Option<Format>,
+}
+
+impl Args {
+    pub fn validate(&self) -> (Format, Format) {
+        let mut cmd = Self::command();
+        if self.in_path.is_none() && self.in_format.is_none() {
+            cmd.error(
+                ErrorKind::MissingRequiredArgument,
+                "must specify either an input file or an input format",
+            )
+            .exit();
+        }
+
+        if self.out_path.is_none() && self.out_format.is_none() {
+            cmd.error(
+                ErrorKind::MissingRequiredArgument,
+                "must specify either an output file or an output format",
+            )
+            .exit();
+        }
+
+        if let Some(ref path) = self.in_path {
+            if !path.exists() || !path.is_file() {
+                cmd.error(
+                    ErrorKind::ValueValidation,
+                    "input file does not exist or is not a file",
+                )
+                .exit();
+            }
+        }
+
+        let input_format = match get_format(self.in_path.as_ref(), self.in_format.as_ref()) {
+            Some(format) => format,
+            None => cmd
+                .error(
+                    ErrorKind::ValueValidation,
+                    "couldn't determine input format from file extension",
+                )
+                .exit(),
+        };
+
+        let output_format = match get_format(self.out_path.as_ref(), self.out_format.as_ref()) {
+            Some(format) => format,
+            None => cmd
+                .error(
+                    ErrorKind::ValueValidation,
+                    "couldn't determine output format from file extension",
+                )
+                .exit(),
+        };
+
+        (input_format, output_format)
+    }
+}
+
+fn get_format(path: Option<&PathBuf>, format: Option<&Format>) -> Option<Format> {
+    if let Some(format) = format {
+        Some(*format)
+    } else {
+        path.and_then(|path| {
+            path.extension()
+                .and_then(|ext| Format::from_str(ext.to_str()?).ok())
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn verify_cli() {
+        Args::command().debug_assert();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::{io::Read, path::PathBuf, str::FromStr};
+use std::io::Read;
 
-use anyhow::Context;
 use clap::Parser;
 use format::Format;
 
@@ -9,27 +8,7 @@ mod format;
 
 fn main() -> anyhow::Result<()> {
     let args = cli::Args::parse();
-
-    // TODO: can this be done directly in clap?
-    if args.in_path.is_none() && args.in_format.is_none() {
-        eprintln!("Error: either --in-path or --in-format must be specified");
-        std::process::exit(1);
-    }
-    if args.out_path.is_none() && args.out_format.is_none() {
-        eprintln!("Error: either --out-path or --out-format must be specified");
-        std::process::exit(1);
-    }
-
-    if let Some(ref path) = args.in_path {
-        if !path.exists() || !path.is_file() {
-            eprintln!("Error: input file does not exist or is not a file");
-
-            std::process::exit(1);
-        }
-    }
-
-    let input_format = get_format(args.in_path.as_ref(), args.in_format.as_ref())
-        .context("Couldn't determine input format")?;
+    let (input_format, output_format) = args.validate();
 
     let input = match args.in_path {
         Some(ref path) => std::fs::read_to_string(path)?,
@@ -41,9 +20,6 @@ fn main() -> anyhow::Result<()> {
     };
 
     let serde_value = to_value(&input, input_format)?;
-
-    let output_format = get_format(args.out_path.as_ref(), args.out_format.as_ref())
-        .context("Couldn't determine output format")?;
 
     let output = match output_format {
         Format::JSON => serde_json::to_string_pretty(&serde_value)?,
@@ -73,16 +49,5 @@ fn to_value(input: &str, input_format: Format) -> anyhow::Result<Box<dyn erased_
             let value: toml::Value = toml::from_str(input)?;
             Ok(Box::new(value))
         }
-    }
-}
-
-fn get_format(path: Option<&PathBuf>, format: Option<&Format>) -> Option<Format> {
-    if let Some(format) = format {
-        Some(*format)
-    } else {
-        path.and_then(|path| {
-            path.extension()
-                .and_then(|ext| Format::from_str(ext.to_str()?).ok())
-        })
     }
 }
