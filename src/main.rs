67cc92e521bebc62b276e94ec71f95c783946d2e
fedfontana <federico.fontana2000@gmail.com>
2024-02-23 13:21:40 +0100

    changes

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,12 @@ fn main() -> anyhow::Result<()> {
     let args = cli::Args::parse();
     let (input_format, output_format) = args.validate();
 
-    let input = match args.in_path {
-        Some(ref path) => std::fs::read_to_string(path)?,
-        None => {
-            let mut buf = String::new();
-            std::io::stdin().read_to_string(&mut buf)?;
-            buf
-        }
+    let mut input: Box<dyn Read> = match args.in_path {
+        Some(ref path) => Box::new(std::fs::File::open(path)?),
+        None => Box::new(std::io::stdin()),
     };
 
-    let serde_value = to_value(&input, input_format)?;
+    let serde_value = to_value(&mut input, input_format)?;
 
     let output = match output_format {
         Format::JSON => serde_json::to_string_pretty(&serde_value)?,
@@ -35,18 +31,23 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn to_value(input: &str, input_format: Format) -> anyhow::Result<Box<dyn erased_serde::Serialize>> {
+fn to_value(
+    input: &mut impl Read,
+    input_format: Format,
+) -> anyhow::Result<Box<dyn erased_serde::Serialize>> {
     match input_format {
         Format::JSON => {
-            let value: serde_json::Value = serde_json::from_str(input)?;
+            let value: serde_json::Value = serde_json::from_reader(input)?;
             Ok(Box::new(value))
         }
         Format::YAML => {
-            let value: serde_yaml::Value = serde_yaml::from_str(input)?;
+            let value: serde_yaml::Value = serde_yaml::from_reader(input)?;
             Ok(Box::new(value))
         }
         Format::TOML => {
-            let value: toml::Value = toml::from_str(input)?;
+            let mut toml_string = String::new();
+            input.read_to_string(&mut toml_string)?;
+            let value: toml::Value = toml::from_str(&toml_string)?;
             Ok(Box::new(value))
         }
     }
